900563a502e057c05c9c7c8d14f4e5ebbe4c6813
Ednilson Rossi <ednilsonrossi@gmail.com>
2023-03-02 17:38:53 -0300

    app5: projeto iniciado

---
+++ App5_Sorteador/app/src/main/java/br/edu/ifsp/dmos5/app5_sorteador/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.dmos5.app5_sorteador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
